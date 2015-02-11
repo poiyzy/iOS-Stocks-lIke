9e21990c3cf4f1a2ea2ffedf71a55e64852ffd3c
Roy Young <zhuoyuyang@gmail.com>
2015-02-12 00:15:23 +0800

    Polish.
    * Added Yahoo finance api.
    * Added Switch Stocks.
    * Added Switch Period.

--- js/app.js
+++ js/app.js
@@ -1,86 +1,90 @@
-var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+var App;
 
-drawBarChart("#bar-chart", dataset);
-drawLineChart("#line-chart", dataset);
+App = (function() {
 
-function drawBarChart(doomName, data) {
-  var width = $(doomName).width(),
-      height = $(doomName).height(),
-      y = d3.scale.linear().domain([0, d3.max(data)]).range([0, height]),
-      x = d3.scale.linear().domain([0, data.length]).range([0, width])
+  function App() {
+    this.initialize()
+    this.today = new Date();
+    this.periodStartDate = {"1D": moment().subtract(1, "day").format("YYYY-MM-DD"), "1W": moment().subtract(7, "day").format("YYYY-MM-DD"), "1M": moment().subtract(30, "day").format("YYYY-MM-DD"), "3M": moment().subtract(90, "day").format("YYYY-MM-DD"), "6M": moment().subtract(360, "day").format("YYYY-MM-DD"), "1Y": moment().subtract(365, "day").format("YYYY-MM-DD"), "2Y": moment().subtract(730, "day").format("YYYY-MM-DD")}
+  }
 
-  var svg = d3.select(doomName)
-                .append("svg")
-                .attr("width", width)
-                .attr("height", height)
+  App.prototype.initialize = function() {
+    $.ajax({
+      type: "GET",
+      url: "http://query.yahooapis.com/v1/public/yql?",
+      data: {q: "select * from csv where url='http://download.finance.yahoo.com/d/quotes.csv?s=YHOO,GOOG,AAPL,FB,TWTR,BABA&f=sl1d1t1c1ohgv&e=.csv' and columns='symbol,price,date,time,change,col1,high,low,col2'", format: "json"}
+    })
+    .done((function(_this) {
+      return function(data) {
+        stocks = data.query.results.row
+        _this.renderStocksList(stocks);
 
-  svg.selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", function(d, i) {
-        return x(i);
-      })
-      .attr("y", function(d) {
-        return height - y(d);
-      })
-      .attr("width", width / data.length - 2)
-      .attr("height", function(d) {
-        return y(d);
-      })
-      .attr("fill", "#363636")
-      .attr("opacity", .85);
-}
+        _this.retriveHistoryData(stocks[0].symbol, _this.periodStartDate["6M"]);
+      };
+    })(this));
+  };
 
-function drawLineChart(doomName, data) {
-  var width = $(doomName).width(),
-      height = $(doomName).height(),
-      marginTop = 20;
+  App.prototype.renderStocksList = function(data) {
+    var html = '';
 
-  var x = d3.scale.linear().range([0, width]),
-      y = d3.scale.linear().range([height, marginTop]);
+    $.each(data, function(i, d) {
+      html += '<div class="row stock'
+      if (i == 0) {
+        html += " active"
+      }
+      html += '" data-stock-symbol="' + d.symbol + '">'
+      html += '<div class="col-xs-4">' + d.symbol + '</div>'
+      html += '<div class="col-xs-4">' + d.price + '</div>'
+      html += '<div class="col-xs-4">'
+      html += d.change.match(/\+/) ? '<span class="label label-success">' : '<span class="label label-danger">'
+      html += d.change + '</span>'
+      html += '</div>'
+      html += '</div>'
+    })
 
-  var xAxis = d3.svg.axis()
-                    .scale(x)
-                    .orient("bottom");
+    $(".stockList").html(html);
 
-  var yAxis = d3.svg.axis()
-                    .scale(y)
-                    .orient("right");
+    this.enableClickToSwitch('.stock');
+    this.enableClickToSwitch('.period');
+  };
 
-  var area = d3.svg.area()
-                .x(function(d, i) { return x(i); })
-                .y0(height)
-                .y1(function(d) { return y(d); });
+  App.prototype.retriveHistoryData = function(symbol, startDate) {
+    $("#line-chart").html(null)
+    $("#bar-chart").html(null)
+    $("#graph-place-holder").html("Retreving Chart...")
 
-  var svg = d3.select(doomName).append("svg")
-                .attr("width", width)
-                .attr("height", height)
-                .append("g")
-                .attr("transform", "translate(5, -20)")
-                .attr("fill", "#363636")
-                .attr("opacity", .85)
+    $.ajax({
+      type: "GET",
+      url: "http://query.yahooapis.com/v1/public/yql?",
+      data: {q: 'select * from yahoo.finance.historicaldata where symbol = "' + symbol + '" and startDate = "' + startDate + '" and endDate = "' + '2015-02-11' + '"', format: 'json', diagnostics: true, env: 'store://datatables.org/alltableswithkeys'}
+    })
+    .done((function(_this) {
+      return function(data) {
+        $("#graph-place-holder").html(null);
 
-  x.domain([0, data.length]);
-  y.domain([0, d3.max(data, function(d) { return d; })]);
+        stockPrices = data.query.results.quote
 
-  svg.append("path")
-      .datum(data)
-      .attr('class', 'area')
-      .attr("d", area)
+        chart = new Chart;
+        chart.drawBarChart(stockPrices);
+        chart.drawLineChart(stockPrices);
+      };
+    })(this));
+  };
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+  App.prototype.enableClickToSwitch = function(target) {
+    $(target).click((function(_this) {
+      return function(e) {
+        $(target).removeClass("active")
+        $(e.currentTarget).addClass("active")
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform","translate(" + (width - 10) + ", 0)")
-      .call(yAxis)
-      .append("text")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Price ($)");
-}
+        stockSymbol = $('.stock.active').data("stockSymbol")
+        startDate = _this.periodStartDate[$('.period.active').html()]
+        _this.retriveHistoryData(stockSymbol, startDate);
+      }
+    })(this));
+  };
+
+  return App;
+})();
+
+app = new App;
